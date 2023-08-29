0413d17cad67dadf64d1ce05567a9955bdb8f2ed
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-08-29 22:53:14 +0600

    TCGA BRCA

---
+++ tcga_data_downloads/getData.R
@@ -0,0 +1,57 @@
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+library(dplyr)
+library(tidyverse)
+library(pheatmap)
+library(maftools)
+
+
+#geta a list of project
+gdcproject <- getGDCprojects()
+getProjectSummary('TCGA-BRCA')
+
+#building a query
+query.TCGA <- GDCquery(project = 'TCGA-BRCA', 
+                       data.category = 'Transcriptome Profiling')
+output_query.TCGA <- getResults(query.TCGA)
+
+#build a query to retrieve gene expression
+query.TCGA <- GDCquery(project = 'TCGA-BRCA', 
+                       data.category = 'Transcriptome Profiling',
+                       experimental.strategy = 'RNA-Seq',
+                       workflow.type = 'STAR - Counts',
+                       access = 'open',
+                       barcode = c('TCGA-OL-A5D6-01A-21R-A27Q-07', 
+                                   'TCGA-AN-A0FT-01A-11R-A034-07', 
+                                   'TCGA-B6-A0RH-01A-21R-A115-07'))
+getResults(query.TCGA)
+
+#download data GDCdownloads
+
+GDCdownload(query.TCGA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tcga_data_downloads/installPackage.R
@@ -0,0 +1,52 @@
+library(dplyr)
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+
+if (!dir.exists('Data')) {
+  dir.create('Data')
+}
+
+# Downloads TCGA-BRCA expression data
+query.exp <- GDCquery(project = "TCGA-BRCA",
+                      data.category = "Transcriptome Profiling",
+                      data.type = "Gene Expression Quantification", 
+                      workflow.type = "STAR - Counts")
+GDCdownload(query.exp)
+dataExpression <- GDCprepare(query.exp)
+
+# Downloads patient, drug, and radiation data
+query.clin <- GDCquery(project = "TCGA-BRCA",
+                       data.category = "Clinical",
+                       file.type = "xml") 
+GDCdownload(query.clin)
+clinical.patient <- GDCprepare_clinic(query.clin, clinical.info = "patient")
+clinical.drug <- GDCprepare_clinic(query.clin, clinical.info = "drug")
+clinical.radiation <- GDCprepare_clinic(query.clin, clinical.info = "radiation")
+
+write.csv(clinical.patient, './Data/clinical_patient.csv')
+write.csv(clinical.drug, './Data/clinical_drug.csv')
+write.csv(clinical.radiation, './Data/clinical_radiation.csv')
+
+# Separates clinical, expression, and annotation data
+clinicalData <- as.data.frame(colData(dataExpression))
+expData <- as.data.frame(assay(dataExpression))
+annotData <- as.data.frame(rowRanges(dataExpression))
+
+clinicalData <- select(clinicalData, -c(treatments, disease_type, primary_site))
+expData <- select(expData, clinicalData$barcode)
+
+# Saves intermediate data
+write.csv(annotData, './Data/annotations.csv')
+write.csv(clinicalData, "./Data/clinical.csv")
+write.csv(expData, "./Data/expression.csv")
+
+# Clears memory
+rm(list = ls())
+
+
+install.packages("maftools")
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("maftools")
